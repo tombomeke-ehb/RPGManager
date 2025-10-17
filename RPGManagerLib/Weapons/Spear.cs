b02f3d9adf188fd109d5ad4dfca56a6d4784b92c
tombomeke-ehb <tom.dekoning@student.ehb.be>
2025-10-17 22:21:34 +0200

    Moved files to different location
    Made small changes to make Spear class more readable

--- RPGManagerLib/Weapons/Spear.cs
+++ RPGManagerLib/Weapons/Spear.cs
@@ -3,15 +3,15 @@
     public class Spear : Weapon
     {
         public Spear() : base
-            (17,
-            120,
-            Rarity.COMMON,
-            1,
-            "Basic Spear",
-            WeaponType.SPEAR,
-            Element.NONE,
-            2,
-            InventorySpaceAmount.LARGE) { }
+            (damageAmount: 17,
+            durability: 120,
+            rarity: Rarity.COMMON,
+            level: 1,
+            name: "Basic Spear",
+            weaponType: WeaponType.SPEAR,
+            element: Element.NONE,
+            cooldownTime: 2,
+            inventorySpaceAmount: InventorySpaceAmount.LARGE) { }
 
         public Spear(int damageAmount, int durability, Rarity rarity, int level, string name, Element element, double cooldownTime, InventorySpaceAmount inventorySpaceAmount)
             : base(damageAmount, durability, rarity, level, name, WeaponType.SPEAR, element, cooldownTime, inventorySpaceAmount)
