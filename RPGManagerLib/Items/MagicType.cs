ec9f515048470433aca2f84bce75ce20fd858985
tombomeke-ehb <tom.dekoning@student.ehb.be>
2026-02-16 02:06:31 +0100

    Create MagicType.cs
    Made an ENUM for all type of MagicTypes will be connected to spells and staffs.

---
+++ RPGManagerLib/Items/MagicType.cs
@@ -0,0 +1,14 @@
+﻿namespace RPGManagerLib.Items
+{
+    public enum MagicType
+    {
+        FIRE,
+        ICE,
+        LIGHTNING,
+        EARTH,
+        WIND,
+        WATER,
+        LIGHT,
+        DARK
+    }
+}
