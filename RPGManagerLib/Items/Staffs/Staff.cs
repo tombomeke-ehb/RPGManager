d181a25b84f0d40034f2b1a267c6a4e417e8c52b
tombomeke-ehb <tom.dekoning@student.ehb.be>
2026-02-16 02:06:56 +0100

    Add Staff equipable class
    Add Staff.cs to RPGManagerLib/Items/Staffs implementing IEquipable. The Staff class exposes Name, Rarity and MagicType, and defines InventorySpaceAmount as LARGE and EquipableType as STAFF, with a constructor to initialize name, rarity and magic type.

---
+++ RPGManagerLib/Items/Staffs/Staff.cs
@@ -0,0 +1,29 @@
+﻿using RPGManagerLib.Items.Weapons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGManagerLib.Items.Staffs
+{
+    public class Staff : IEquipable
+    {
+        public string Name { get; set; }
+
+        public Rarity Rarity { get; set; }
+
+        public InventorySpaceAmount InventorySpaceAmount => InventorySpaceAmount.LARGE;
+
+        public EquipableType EquipableType => EquipableType.STAFF;
+
+        public MagicType MagicType { get; set; }
+
+        public Staff(string name, Rarity rarity, MagicType magicType)
+        {
+            Name = name;
+            Rarity = rarity;
+            MagicType = magicType;
+        }
+    }
+}
