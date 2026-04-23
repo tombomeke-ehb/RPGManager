7913635d1e49acccba31c2a30e5f380eb78e9f13
tombomeke-ehb <tom.dekoning@student.ehb.be>
2026-04-23 14:33:31 +0200

    Add MediumQuiver item class
    Introduce MediumQuiver as a concrete Quiver subclass in RPGManagerLib.Items.Weapons.Quivers. The constructor sets name to "Medium Quiver", rarity to Rarity.UNCOMMON, inventorySpaceAmount to InventorySpaceAmount.SMALL, and capacity to 30, providing a medium-capacity quiver item for the game.

---
+++ RPGManagerLib/Items/Weapons/Quivers/MediumQuiver.cs
@@ -0,0 +1,16 @@
+﻿using RPGManagerLib.Weapons.Quivers;
+
+namespace RPGManagerLib.Items.Weapons.Quivers
+{
+    public class MediumQuiver : Quiver
+    {
+        public MediumQuiver()
+            : base(name: "Medium Quiver",
+                    rarity: Rarity.UNCOMMON,
+                    inventorySpaceAmount: InventorySpaceAmount.SMALL,
+                    capacity: 30)
+        {
+
+        }
+    }
+}
