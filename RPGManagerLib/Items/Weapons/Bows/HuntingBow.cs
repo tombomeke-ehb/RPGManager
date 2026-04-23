97efd5640c8d283a9bd054116653e2e5b79032d9
tombomeke-ehb <tom.dekoning@student.ehb.be>
2026-04-23 14:32:37 +0200

    Add HuntingBow weapon class
    Introduce an internal HuntingBow class (RPGManagerLib/Items/Weapons/Bows/HuntingBow.cs) that subclasses Bow and supplies preset stats: damage 15, durability 130, rarity UNCOMMON, level 5, name "Hunting Bow", weaponType HUNTINGBOW, element NONE, and large inventory space.

---
+++ RPGManagerLib/Items/Weapons/Bows/HuntingBow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGManagerLib.Items.Weapons.Bows
+{
+    internal class HuntingBow : Bow
+    {
+        public HuntingBow()
+            : base(
+                  damageAmount: 15,
+                  durability: 130,
+                  rarity: Rarity.UNCOMMON,
+                  level: 5,
+                  name: "Hunting Bow",
+                  weaponType: WeaponType.HUNTINGBOW,
+                  element: Element.NONE,
+                  inventorySpaceAmount: InventorySpaceAmount.LARGE) { }
+    }
+}
