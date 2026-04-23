b8f11aa2930293470ac2e3d7acdfafbb411a5cb8
tombomeke-ehb <tom.dekoning@student.ehb.be>
2026-04-23 14:32:50 +0200

    Add WarBow weapon class
    Introduce an internal WarBow class in RPGManagerLib.Items.Weapons.Bows that derives from Bow. The constructor initializes weapon stats (damage 25, durability 160, rarity RARE, level 10), sets the name to "War Bow", weaponType to WeaponType.WARBOW, element NONE, and large inventory space. Adds the new WarBow.cs file to the codebase.

---
+++ RPGManagerLib/Items/Weapons/Bows/WarBow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGManagerLib.Items.Weapons.Bows
+{
+    internal class WarBow : Bow
+    {
+        public WarBow()
+            : base(
+                  damageAmount: 25,
+                  durability: 160,
+                  rarity: Rarity.RARE,
+                  level: 10,
+                  name: "War Bow",
+                  weaponType: WeaponType.WARBOW,
+                  element: Element.NONE,
+                  inventorySpaceAmount: InventorySpaceAmount.LARGE) { }
+    }
+}
